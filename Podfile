748f3f3b0551963650ca1204d886d587934cfb3b
SgtGirthquake <makemefoodhoe@aim.com>
2017-06-27 20:34:49 -0400

    Spreadsheet update W/ Issues
    Issues with Spreadsheet

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '10.0'
+
+use_frameworks!
+
+target 'RustMath' do
+
+pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+pod 'SpreadsheetView' , :git => 'https://github.com/kishikawakatsumi/SpreadsheetView.git'
+
+end
--- RustMath/DetailsViewController.swift
+++ RustMath/DetailsViewController.swift
@@ -7,65 +7,134 @@
 //
 
 import UIKit
+import SpreadsheetView
 
 
+class DetailsViewController: UIViewController, SpreadsheetViewDataSource, SpreadsheetViewDelegate {
 
-class DetailsViewController: UIViewController {
-
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var damageLabel: UILabel!
-    @IBOutlet weak var quantityLabel: UILabel!
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var spreadsheetView: SpreadsheetView!
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var itemLabel: UILabel!
     
-    
+  var header = [(String)]()
+  var data = [(String)]()
   var passedData = ""
+    
+    enum Sorting {
+        case ascending
+        case descending
+        
+        var symbol: String {
+            switch self {
+            case .ascending:
+                return "\u{25B2}"
+            case .descending:
+                return "\u{25BC}"
+        }
+      }
+    }
+    
+    var sortedColumn = (column: 0, sorting: Sorting.ascending)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-       
-    
+     
         itemLabel.text = passedData
         myImageView.image = UIImage(named: passedData + ".png")
         
         
-    
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
+        
+        
+        
+        
+        
+       ///// SPREADSHEET FUNCTIONS AND DATA FROM HERE ON ///////
+        spreadsheetView.dataSource = self
+        spreadsheetView.delegate = self
    
-    }
- 
     
-   /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        spreadsheetView.register(HeaderCell.self, forCellWithReuseIdentifier: String(describing: HeaderCell.self))
+        spreadsheetView.register(TextCell.self, forCellWithReuseIdentifier: String(describing: TextCell.self))
+        
+        let data = try! String(contentsOf: Bundle.main.url(forResource: "data", withExtension: "tsv")!, encoding: .utf8)
+            .components(separatedBy: "\r\n")
+            .map { $0.components(separatedBy: "\t") }
+        header = data[0]
+        self.data = Array(data.dropFirst())
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        spreadsheetView.flashScrollIndicators()
+    }
+    
+    // MARK: DataSource ///////
+    
+    func numberOfColumns(in spreadsheetView: SpreadsheetView) -> Int {
+        return header.count
+    }
+    
+    func numberOfRows(in spreadsheetView: SpreadsheetView) -> Int {
+        return 1 + data.count
+    }
+    
+    func spreadsheetView(_ spreadsheetView: SpreadsheetView, widthForColumn column: Int) -> CGFloat {
+        return 80
+    }
+    
+    func spreadsheetView(_ spreadsheetView: SpreadsheetView, heightForRow row: Int) -> CGFloat {
+        if case 0 = row {
+            return 60
+        } else {
+            return 44
+        }
+    }
+    
+    func frozenRows(in spreadsheetView: SpreadsheetView) -> Int {
+        return 1
+    }
+    
+    func spreadsheetView(_ spreadsheetView: SpreadsheetView, cellForItemAt indexPath: IndexPath) -> Cell? {
+        if case 0 = indexPath.row {
+            let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: String(describing: HeaderCell.self), for: indexPath) as! HeaderCell
+            cell.label.text = header[indexPath.column]
+            
+            if case indexPath.column = sortedColumn.column {
+                cell.sortArrow.text = sortedColumn.sorting.symbol
+            } else {
+                cell.sortArrow.text = ""
+            }
+            cell.setNeedsLayout()
+            
+            return cell
+        } else {
+            let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: String(describing: TextCell.self), for: indexPath) as! TextCell
+            cell.label.text = data[indexPath.row - 1][indexPath.column]
+            return cell
+        }
+    }
+    
+    /// Delegate
+    
+    func spreadsheetView(_ spreadsheetView: SpreadsheetView, didSelectItemAt indexPath: IndexPath) {
+        if case 0 = indexPath.row {
+            if sortedColumn.column == indexPath.column {
+                sortedColumn.sorting = sortedColumn.sorting == .ascending ? .descending : .ascending
+            } else {
+                sortedColumn = (indexPath.column, .ascending)
+            }
+            data.sort {
+                let ascending = $0[sortedColumn.column] < $1[sortedColumn.column]
+                return sortedColumn.sorting == .ascending ? ascending : !ascending
+            }
+            spreadsheetView.reloadData()
+        }
+    }
+}
+
+
 
 
